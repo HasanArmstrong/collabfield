b5b9cbae705c32ac2b87e8a4a3e88c178ec42cbb
Hasan Armstrong <hasan_armstrong@hotmail.co.uk>
2019-10-02 21:27:33 +0700

    Add CSS to posts on the home page - add CSS to the posts.scss file - add CSS to the mobile.scss to fix too large text issues on smaller screens

--- Gemfile
+++ Gemfile
@@ -30,7 +30,6 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass', '~> 3.3.6'
-gem 'jquery-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'devise'
@@ -42,7 +41,7 @@ gem 'headless'
 gem 'poltergeist'
 gem 'database_cleaner'
 gem 'faker'
-
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/assets/javascripts/posts/style.js
+++ app/assets/javascripts/posts/style.js
@@ -1,4 +1,7 @@
-$(document).on('turbolinks:load', function() {
+// require jquery
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    $(document).on('turbolinks:load', function() {
     if ($(".single-post-card").length) {
         // set a solid background color style
         if (mode == 1) {
@@ -25,13 +28,13 @@ $(document).on('turbolinks:load', function() {
         $(this).css('border-color', 'rgba(0, 0 , 0, 0.05)');
     });
 
-});
+    });
 
-var colorSet = randomColorSet();
-var mode = Math.floor(Math.random() * 2);
+    var colorSet = randomColorSet();
+    var mode = Math.floor(Math.random() * 2);
 
-// Randomly returns a color scheme
-function randomColorSet() {
+    // Randomly returns a color scheme
+    function randomColorSet() {
     var colorSet1 = ['#45CCFF', '#49E83E', '#FFD432', '#E84B30', '#B243FF'];
     var colorSet2 = ['#FF6138', '#FFFF9D', '#BEEB9F', '#79BD8F', '#79BD8F'];
     var colorSet3 = ['#FCFFF5', '#D1DBBD', '#91AA9D', '#3E606F', '#193441'];
@@ -41,10 +44,12 @@ function randomColorSet() {
     var colorSet7 = ['#405952', '#9C9B7A', '#FFD393', '#FF974F', '#F54F29'];
     var randomSet = [colorSet1, colorSet2, colorSet3, colorSet4, colorSet5, colorSet6, colorSet7];
     return randomSet[Math.floor(Math.random() * randomSet.length )];
-}
+    }
 
-// Randomly returns a color from an array of colors
-function randomColor() {
+    // Randomly returns a color from an array of colors
+    function randomColor() {
     var color = colorSet[Math.floor(Math.random() * colorSet.length)];
     return color;
-}
+    }
+
+});
