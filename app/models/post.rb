30767c7a09b4970d2aa0101b5d0d362779049998
Hasan Armstrong <hasan_armstrong@hotmail.co.uk>
2019-10-20 19:55:14 +0700

    Define a default_scope for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-    #Every post is going to belong to a category and it's author (user)
-    belongs_to :user
-    belongs_to :category
+  #Every post is going to belong to a category and it's author (user)
+  default_scope -> { includes(:user).order(created_at: :desc) }
+  belongs_to :user
+  belongs_to :category
 end
